a29ae4631001536be15e0f5064d558ef37300ebf
neverorfrog <97flavio.maiorana@gmail.com>
2024-11-01 11:24:40 +0100

    stupid mistake

--- drl_continous/agents/buffer.py
+++ drl_continous/agents/buffer.py
@@ -5,7 +5,7 @@ import gymnasium as gym
 import numpy as np
 import torch
 
-device = torch.device("cpu")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 random.seed(123)
 np.random.seed(123)
